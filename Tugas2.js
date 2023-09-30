1122678027273e38c95361b15f1cdc9eec985e3d
Your Name <you@example.com>
2023-09-30 23:17:08 +0700

    first commit

---
+++ Tugas2.js
@@ -0,0 +1,14 @@
+// Deklarasi variabel
+const firstName = "Haikal";
+const middleName = "Muhammad";
+const lastName = "Kurniawan";
+const age = 20;
+const isMarried = false;
+
+// Menampilkan data
+console.log("Nama Panjang: " + firstName + middleName + lastName);
+console.log("Nama depan: " + firstName);
+console.log("Nama menampil: " + middleName);
+console.log("Nama belakang: " + lastName);
+console.log("Umur: " + age);
+console.log("Status pernikahan: " + (isMarried ? "Menikah" : "Belum menikah"));
